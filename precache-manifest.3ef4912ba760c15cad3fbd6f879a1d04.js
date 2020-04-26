79228a9fe290dae4a72849e84bba40030e46de1c
Esteban Camargo <estebmaister@gmail.com>
2020-04-25 21:42:45 -0400

    Updates

---
+++ precache-manifest.3ef4912ba760c15cad3fbd6f879a1d04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8454364f8538c38e4e8c2d6aa47c86",
+    "url": "/quoter-machine/index.html"
+  },
+  {
+    "revision": "da4ea65ee904dd86280a",
+    "url": "/quoter-machine/static/css/main.8b77e14d.chunk.css"
+  },
+  {
+    "revision": "e59539ac80046a68a693",
+    "url": "/quoter-machine/static/js/2.5b9f9277.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quoter-machine/static/js/2.5b9f9277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da4ea65ee904dd86280a",
+    "url": "/quoter-machine/static/js/main.1636f79e.chunk.js"
+  },
+  {
+    "revision": "f5f37a2abda7d8f4d5a3",
+    "url": "/quoter-machine/static/js/runtime-main.9489f9f4.js"
+  },
+  {
+    "revision": "cd2fd5e73ac34e7f60a68ac6e75e7537",
+    "url": "/quoter-machine/static/media/logo.cd2fd5e7.svg"
+  }
+]);
